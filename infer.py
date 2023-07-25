67147fb5d9dc790248a3e31bf44546ed65298e7b
hrushik98 <91076764+hrushik98@users.noreply.github.com>
2023-07-25 15:09:46 +0530

    Update infer.py

--- infer.py
+++ infer.py
@@ -1,12 +1,17 @@
 import argparse
 import torch
-from transformers import LoraConfig
+
 from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig, AutoTokenizer
 model_name = "TinyPixel/Llama-2-7B-bf16-sharded"
 tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
 tokenizer.pad_token = tokenizer.eos_token
 
 from peft import LoraConfig, get_peft_model
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig, AutoTokenizer
+
+model_name = "TinyPixel/Llama-2-7B-bf16-sharded"
+
 
 
 def main():
@@ -18,6 +23,19 @@ def main():
     text = args.text
     device = args.device
 
+    bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.float16,
+)
+
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name,
+        quantization_config=bnb_config,
+        trust_remote_code=True
+    )
+    model.config.use_cache = False
+
     lora_config = LoraConfig.from_pretrained('outputs')
     model = get_peft_model(model, lora_config)
 
@@ -25,8 +43,8 @@ def main():
     outputs = model.generate(**inputs, max_new_tokens=50)
     generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
 
-    print("Input Text: ", text)
-    print("Generated Text: ", generated_text)
+    print("Human: ", text)
+    print("Assistant: ", generated_text)
 
 if __name__ == "__main__":
     main()
